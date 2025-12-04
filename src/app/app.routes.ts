7d9234df94de0c4172a7565ba9a8ca8777c38fff
Keroleum <kerosensible@gmail.com>
2025-12-04 08:54:04 +0800

    feat: implementing profile details, removed home page as login is defaulted as login page, fixed approutes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
 import { LoginPage } from './login-page/login-page';
 import { EventsPage } from './events-page/events-page';
 import { EventDetails } from './event-details/event-details';
@@ -7,9 +6,8 @@ import { CreateEventsPage } from './create-events-page/create-events-page';
 
 export const routes: Routes = [
 
-    {path: '', component: Home},
-    {path: 'login-page', component: LoginPage},
+    {path: '', component: LoginPage},
     {path: 'events', component: EventsPage},
-    {path: 'event-details/:id', component: EventDetails},
+    {path: 'event-details', component: EventDetails},
     {path: 'create-event', component: CreateEventsPage}
 ];
